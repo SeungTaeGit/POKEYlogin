94a861c383caba3134f887d612351fe6781a7509
SeungTaeGit <129585999+SeungTaeGit@users.noreply.github.com>
2023-05-26 22:57:16 +0900

    Add files via upload

---
+++ POKEYlogin/src/main/java/capstone/pokey/POKEYlogin/PokeYloginApplication.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeYloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeYloginApplication.class, args);
+	}
+
+}
